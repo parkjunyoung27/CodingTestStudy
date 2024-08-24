1cf1d0ee128917c93211b338fc081942e3e07fef
parkjunyoung <hpaaycim1@naver.com>
2024-08-25 01:23:23 +0900

    확장 유클리드 호제법 공부

--- src/chap07/P21568_AxByC.java
+++ src/chap07/P21568_AxByC.java
@@ -6,26 +6,29 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
-	
+public class P21568_AxByC {
 	public static void main(String[] args) throws Exception{
-			
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		// 1, 2, 3 번째 수 
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		int c = Integer.parseInt(st.nextToken());
 		
+		// 최대 공약수 계산
 		long gcd = gcd(a,b);
 		if(c % gcd != 0) { //최대공약수 의 배수가 아니라면 종료
 			System.out.println(-1);
 		} else {
 			int mok = (int) (c / gcd);
-			long[] ret = Excute(a, b);
+			long[] ret = Excute(a, b); // 나머지가 0 이 될 때 까지 재귀함수
 			System.out.println(ret[0] * mok + " " + ret[1] * mok);
 		}
+		
+		bw.close();
 		
 	}
 	
